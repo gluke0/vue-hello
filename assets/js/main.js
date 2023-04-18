ef16238dadbc81f6d95923740f2bcbb1aba20c46
gluke0 <milanesigianluca@gmail.com>
2023-04-18 16:46:17 +0200

    aggiunta bottoni cambio colore e filtro immagine

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,27 @@ const { createApp } = Vue
 
 createApp({
     data() {
-      return {
-        message: "Ciao, Hello, Hola Vue!",
-        randomImage:  "https://picsum.photos/200/300"
-      }
+        return {
+            message: "Ciao, Hello, Hola Vue!",
+            randomImage:  "https://picsum.photos/200/300",
+            color: "black",
+            filter: "blurred"
+        }
     },
+    methods:{
+        changeColor(){
+            if ( this.color == "black") {
+                this.color = "red"
+            } else {
+                this.color = "black"
+            }
+        },
+        removeFilter(){
+            if ( this.filter == "blurred") {
+                this.filter = "noblurred"
+            } else {
+                this.filter = "blurred"
+            }
+        },
+    }
 }).mount('#app')
